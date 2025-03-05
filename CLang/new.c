646f036108c3eb9988dacbff1a7ceaf449f2dc0d
Avinash <53809765+avinash7375@users.noreply.github.com>
2025-03-05 18:48:08 +0530

    put new.c

---
+++ CLang/new.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+//tryina create a program to do something that is not legal, but it's legal
+//let's do that thing
+
+int main(void) {
+    printf("Shut the hell up, bitch!!!!");
+}

2cde8e1be0afd2c734a6139618ff7b0341ab396e
Avinash <53809765+avinash7375@users.noreply.github.com>
2025-03-05 19:49:08 +0530

    Create make_and_insert.sql

---
+++ MySQL/make_and_insert.sql
@@ -0,0 +1,14 @@
+CREATE TABLE more(
+  sid INT,
+  sname VARCHAR(50);
+  date_mem DATE
+);
+
+show tables;
+
+desc table_name;
+
+
+-- inserting values into tables
+INSERT INTO table_name
+VALUES (val1, val2, ...........);

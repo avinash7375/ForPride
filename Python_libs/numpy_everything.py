cb295cc71b8d6be5cceaff79df04db5cb8d712bb
Avinash <53809765+avinash7375@users.noreply.github.com>
2025-03-15 19:22:34 +0530

    Update numpy_everything.py

--- Python_libs/numpy_everything.py
+++ Python_libs/numpy_everything.py
@@ -1,1 +1,8 @@
 print("This Is For NUMPY")
+
+import numpy as np
+# Creating multi-dimensional arrays
+a1D = np.array([1, 2, 3, 4])
+a2D = np.array([[1, 2], [3, 4]])
+a3D = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
+

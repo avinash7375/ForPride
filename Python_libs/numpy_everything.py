4c5d460657653619a9ad4a21d153fa8dd70b9c10
Avinash <53809765+avinash7375@users.noreply.github.com>
2025-03-15 19:24:48 +0530

    Update numpy_everything.py

--- Python_libs/numpy_everything.py
+++ Python_libs/numpy_everything.py
@@ -9,3 +9,7 @@ a3D = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
 # Creating different sized arrsys
 np.array([127, 128, 129], dtype=np.int8)    # int16, int32, int64
 
+# using arange function
+np.arange(10)
+np.arange(2, 10, dtype=float)
+np.arange(2, 3, 0.1)

e0b554dc287ca7692ba106d10fa23bfc8230b5a1
Avinash <53809765+avinash7375@users.noreply.github.com>
2025-03-17 19:03:22 +0530

    Create dtypes.c

---
+++ CLang/dtypes.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;           // Integer
+    float b = 5.5;        // Float
+    char c = 'A';         // Character
+    double d = 12.3456;   // Double
+    
+    printf("Integer: %d\n", a);
+    printf("Float: %.2f\n", b);
+    printf("Character: %c\n", c);
+    printf("Double: %.4lf\n", d);
+    
+    return 0;
+}
